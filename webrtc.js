7f2d8967a42996bb4185dfdc6bef7dfa085da4bb
Tobi Lehman <tobi.lehman@gmail.com>
2020-05-26 20:23:55 -0700

    Properly await the MediaStream before assigning

--- webrtc.js
+++ webrtc.js
@@ -1,12 +1,16 @@
 const openMediaDevices = async (constraints) => {
-    return await navigator.mediaDevices.getUserMedia(constraints);
+    return navigator.mediaDevices.getUserMedia(constraints);
 }
 
-try {
-    const stream = openMediaDevices({'video':true,'audio':true});
-    console.log('Got MediaStream:', stream);
-    const localVideo = document.querySelector("video#localVideo");
-    localVideo.srcObject = stream;
-} catch(error) {
-    console.error('Error accessing media devices.', error);
+async function playLocalVideo() {
+    try {
+        const stream = await openMediaDevices({ 'video': true, 'audio': true });
+        console.log('Got MediaStream:', stream);
+        const localVideo = document.querySelector("video#localVideo");
+        localVideo.srcObject = stream;
+    } catch (error) {
+        console.error('Error accessing media devices.', error);
+    }
 }
+
+playLocalVideo();
